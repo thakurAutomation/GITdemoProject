f56fd28ea7277d819f830d572201b6caccee0b30
RITESH THAKUR <thakur@gmail.com>
2021-07-24 19:48:15 -0400

    THIS is my first commit....

---
+++ src/test/java/dataDrivenTesting/Test1.java
@@ -0,0 +1,13 @@
+package dataDrivenTesting;
+
+public class Test1 {
+	
+	public static void main(String args[]) {
+	String hunger = "YES";
+	if (hunger.equalsIgnoreCase("YES")) {
+		System.out.println("eat something");
+	}else {
+		System.out.println("i am not hungy");
+	}
+	}
+}
